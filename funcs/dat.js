1f54ccc9bd944846692b45d15f310d79f2092c89
az38 <152063482+p3pfes@users.noreply.github.com>
2025-04-19 23:02:48 +0100

    Create dat.js

---
+++ funcs/dat.js
@@ -0,0 +1,22 @@
+const fs = require('fs') 
+exports.util = function()  {
+    const ret = {
+        "checkdata": function(area) {
+            if (!fs.existsSync(area)) {
+                return false
+            } else {
+                return true
+            }
+
+        },
+        "grabdata": function(message) {
+            if(fs.existsSync(`./saves/${message.guild.id}/${message.mentions.channels.first().id}.json`)) {
+                //I SPENT 30 FUCKIGN MINUTES DEBUGGING THIS AND I ONLY REALISED THAT I MISSPELT "SAVES" AS "SAVE"
+                const data = JSON.parse(fs.readFileSync(`./saves/${message.guild.id}/${message.mentions.channels.first().id}.json`), 'utf8')
+                return data
+            } else {
+            }
+        }
+    }
+    return ret
+}
